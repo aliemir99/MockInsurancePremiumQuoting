22f061a67769df03853bf942b768d883b87be69d
Ali Emir <aliemir1999@gmail.com>
2025-10-16 13:19:20 -0300

    Clean repo : ingore .vs add build config

--- webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+613bd2fd1cbff92e8c5cba6984508e0e2e13cbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
